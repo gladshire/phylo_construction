8d5466393dbf4973f48b0a9b5c50263805507b79
gladshire <miles.girard@gmail.com>
2022-07-28 16:41:55 -0500

    Added chimera detection, refined data retrieval

--- scripts/retrieve_sra_data.py
+++ scripts/retrieve_sra_data.py
@@ -1,13 +1,18 @@
 import os
 import sys
+import re
 import subprocess
+from Bio import Entrez
+
+Entrez.email = "mwoodc2@uic.edu"
+Entrez.api_key = "6512380b5cb6a15be6ef4919564e2e186508"
 
 SRA_TOOLKIT_BIN_PATH = "~/miles/packages/sratoolkit.3.0.0-ubuntu64/bin/"
 SRA_LIST_PATH = "~/miles/allele_phasing/phylo_construction/data_retrieval/sras.txt"
 
 
 def get_sra_numbers(sra_file_path):
-    sra_list_file = open(os.getcwd() + "/sras.txt", 'r')
+    sra_list_file = open("sras.txt", 'r')
     ln = sra_list_file.readline()
     ln = sra_list_file.readline()
     sra_list = []
@@ -15,6 +20,30 @@ def get_sra_numbers(sra_file_path):
         sra_list.append(ln[:-1:])
         ln = sra_list_file.readline()
     return sra_list
+
+
+def get_tax_id(sra_number):
+    handle = Entrez.efetch(db = "sra", id = sra_number)
+    handle_str = str(handle.read())
+    tax_id_fld_pat = re.compile('tax_id="([^"]*)"')
+    tax_id_fld_mat = tax_id_fld_pat.search(handle_str)
+    tax_id_fld_sub = handle_str[tax_id_fld_mat.start():tax_id_fld_mat.end()]
+    tax_id_pat = re.compile('"([^"]*)"')
+    tax_id_mat = tax_id_pat.search(tax_id_fld_sub)
+    taxon_id = tax_id_fld_sub[tax_id_mat.start():tax_id_mat.end()][1:-1:]
+    return taxon_id
+
+
+def get_org(sra_number):
+    handle = Entrez.efetch(db = "sra", id = sra_number)
+    handle_str = str(handle.read())
+    org_fld_pat = re.compile('organism="([^"]*)"')
+    org_fld_mat = org_fld_pat.search(handle_str)
+    org_fld_sub = handle_str[org_fld_mat.start():org_fld_mat.end()]
+    org_pat = re.compile('"([^"]*)"')
+    org_mat = org_pat.search(org_fld_sub)
+    org = org_fld_sub[org_mat.start():org_mat.end()][1:-1:]
+    return org
 
 
 def get_sra_data(sra_list, threads, out_dir_prefetch, out_dir_fastq):
@@ -29,15 +58,21 @@ def get_sra_data(sra_list, threads, out_dir_prefetch, out_dir_fastq):
     if out_dir_fastq[-1] != "/": out_dir_fastq += "/"
 
     for sra in sra_list:
-        print("Downloading: " + sra + "to " + out_dir_prefetch)
+        taxon_id = get_tax_id(sra)
+        organism = get_org(sra)
+        out_file = taxon_id + "." + organism.lower().replace(" ", "_") + ".fastq"
+
+        print("Downloading: " + organism + " (" + taxon_id + ")" + " ...")
         prefetch_cmd = [SRA_TOOLKIT_BIN_PATH + "prefetch", sra,
                         "--max-size", "100g", "--progress",
                         "--output-directory", out_dir_prefetch]
         subprocess.run(" ".join(prefetch_cmd), shell=True)
+
         print("Dumping " + sra + ".sra to fastq ...")
         fasterq_cmd = [SRA_TOOLKIT_BIN_PATH + "fasterq-dump",
                        out_dir_prefetch + sra, "--threads",
-                       str(threads), "--outdir", out_dir_fastq]
+                       str(threads), "--outfile",
+                       out_dir_fastq + out_file]
         subprocess.run(" ".join(fasterq_cmd), shell=True)
     print("Done.")    
 
---
+++ scripts/run_chimera_detection.py
@@ -0,0 +1,146 @@
+import sys
+import os
+import subprocess
+import pandas
+from Bio import SeqIO
+
+SCRIPTS_HOME = os.path.expanduser("~/miles/allele_phasing/phylo_construction/scripts/")
+
+
+def make_blast_db(proteome_ref, out_dir):
+    if out_dir == ".": out_dir = os.getcwd()
+    if os.path.isabs(out_dir) == False: out_dir = os.path.abspath(out_dir)
+    if out_dir[-1] != "/": out_dir += "/"
+
+    path_ref, file_ref = os.path.split(proteome_ref)
+    ref_name = str(file_ref)
+    ref_base_name = ref_name.split(".")[0]
+
+    db_file_1 = ref_base_name + ".phr"
+    db_file_2 = ref_base_name + ".pin"
+    db_file_3 = ref_base_name + ".psq"
+    
+    cmd = ["makeblastdb", "-in", proteome_ref, "-dbtype", "prot", "-out",
+           out_dir + ref_base_name]
+
+    print(" ".join(cmd))
+    subprocess.run(" ".join(cmd), shell = True)
+
+
+def run_blastx(transcripts, blast_db, threads, out_dir):
+    if out_dir == ".": out_dir = os.getcwd()
+    if os.path.isabs(out_dir) == False: out_dir = os.path.abspath(out_dir)
+    if out_dir[-1] != "/": out_dir += "/"
+
+    path_transcript, file_transcript = os.path.split(transcripts)
+    transcript_name = str(file_transcript)
+    transcript_base_name = transcript_name.split(".")[0]
+
+    blastx_output_file = transcript_base_name + ".blastx"
+
+    cmd = ["blastx", "-db", blast_db, "-query", transcripts, "-evalue",
+           "0.01", "-outfmt", "6", "qseqid", "qlen", "sseqid", "slen",
+           "frames", "pident", "nident", "length", "mismatch", "gapopen",
+           "qstart", "qend", "sstart", "send", "evalue", "bitscore", "-out",
+           out_dir + blastx_output_file, "-num_reads", str(threads),
+           "-max_target_seqs", "100"]
+
+    print(" ".join(cmd))
+    subprocess.run(" ".join(cmd), shell = True)
+
+
+def run_chimera_detection(blastx_output_file, out_dir):
+    if out_dir == ".": out_dir = os.getcwd()
+    if os.path.isabs(out_dir) == False: out_dir = os.path.abspath(out_dir)
+    if out_dir[-1] != "/": out_dir += "/"
+
+    path_blastx, file_blastx = os.path.split(blastx_output_file)
+    blastx_name = str(file_blastx)
+    blastx_base_name = blastx_name.split(".")[0]
+
+    cut_file = blastx_base_name + ".cut"
+    info_file = blastx_base_name + ".info"
+
+    cmd = ["python3", SCRIPTS_HOME + "detect_chimera_from_blastx_modified.py",
+           blastx_output_file, out_dir]
+
+    print(" ".join(cmd))
+    subprocess.run(" ".join(cmd), shell = True)
+
+
+def remove_chimeras_from_fasta(transcripts, info_file, out_dir):
+    if out_dir == ".": out_dir = os.getcwd()
+    if os.path.isabs(out_dir) == False: out_dir = os.path.abspath(out_dir)
+    if out_dir[-1] != "/": out_dir += "/"
+
+    path_transcript, file_transcript = os.path.split(transcripts)
+    transcript_name = str(file_transcript)
+    transcript_base_name = transcript_name.split(".")[0]
+
+    filtered_transcripts = transcript_base_name + ".filtered_transcripts.fa"
+    chimeric_transcripts = transcript_base_name + ".chimera_transcripts.fa"
+
+    transcripts_original = SeqIO.index(transcripts, "fasta")
+
+    df = pandas.read_table(info_file, header = None)
+    blastx_columns = "qseqid qlen sseqid slen frames pident nident length mismatch gapopen qstart qend sstart send evalue bitscore empty".strip().split(' ')
+
+    df.columns = blastx_columns
+    chimera_names = df["qseqid"].drop_duplicates()
+    
+    if len(chimera_names) == 0:
+        print("No chimeras found")
+    else:
+        chimeras = []
+        for i in chimera_names:
+            chimeras.append(transcripts_original[i])
+        count = SeqIO.write(chimeras, out_dir + chimeric_transcripts, "fasta")
+        print("Removed {} chimeras".format(count))
+       
+        chimera_list = chimera_names.tolist()
+        all_transcripts_ids = sorted(transcripts_original)
+        non_chimeras_names = [x for x in all_transcripts_ids if x not in chimera_list]
+
+    if len(non_chimeras_names) == 0:
+        print("No non-chimeras found")
+    else:
+        non_chimeras = []
+        for i in non_chimeras_names:
+            non_chimeras.append(transcripts_original[i])
+        count = SeqIO.write(non_chimeras, out_dir + filtered_transcripts, "fasta")
+        print("Retained {} transcripts".format(count))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage:")
+        print("python3 run_chimera_detection.py transcripts_fasta reference_proteome_fasta threads output_directory")
+        sys.exit()
+
+        transcripts = sys.argv[1]
+        proteome_ref = sys.argv[2]
+        threads = sys.argv[3]
+        out_dir = sys.argv[4]
+        
+        if out_dir == ".": out_dir = os.getcwd()
+        if os.path.isabs(out_dir) == False: out_dir = os.path.abspath(out_dir)
+        if out_dir[-1] != "/": out_dir += "/"
+
+        path_ref, file_ref = os.path.split(proteome_ref)
+        ref_name = str(file_ref)
+        ref_base_name = ref_name.split(".")[0]
+
+        path_transcript, file_transcript = os.path.split(transcripts)
+        transcript_name = str(file_transcript)
+        transcript_base_name = transcript_name.plit(".")[0]
+        blastx_output_file = transcript_base_name + ".bastx"
+
+        path_blastx, file_blastx = os.path.split(out_dir + blastx_output_file)
+        blastx_name = str(file_blastx)
+        blastx_base_name = blastx_name.split(".")[0]
+        info_file = blastx_base_name + ".info"
+
+        make_blast_db(proteome_ref, out_dir)
+        run_blastx(transcripts, out_dir + proteome_ref, threads, out_dir)
+        run_chimera_detection(out_dir + blastx_output_file, out_dir)
+        remove_chimeras_from_fasta(transcripts, out_dir + info_file, out_dir)
