08a76d78f01ea319a91e14f2422016ff6686e8e2
gladshire <miles.girard@gmail.com>
2022-09-19 10:17:57 -0500

    Removed test script

--- scripts/blap.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-for file1 in os.listdir("/scratch/miles/allele_phasing/phylo_construction/scripts/05-filter_over_represented"):
-    file_comps = file1.split(".")
-    print(file_comps[0].split("_")[2:4])
